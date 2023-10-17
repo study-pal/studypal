28b8cba586db4b4823ca058b5aa0e4b415aec789
Sophie Dang <101291617+sophiedang0101@users.noreply.github.com>
2023-10-16 19:24:34 -0700

    set up firebase  (#23)

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const { initializeApp, cert, getApps } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./keys/firebase_key.json');
+
+if (getApps().length < 1) {
+   initializeApp({
+      credential: cert(serviceAccount)
+   });
+}
+
+export const db = getFirestore();
