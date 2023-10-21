e6d93d83e37f87eafcfc317a41edf99f5c12e778
Krizza Bullecer <krizzabullecer@gmail.com>
2023-10-21 21:06:11 +0900

    create text input component

---
+++ app/components/forms/TextInput.jsx
@@ -0,0 +1,29 @@
+export default function TextInput({
+  name,
+  value,
+  onChange,
+  onKeyUp,
+  error,
+  placeholder,
+  label,
+  disabled = false,
+}) {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={name} className="font-medium">
+        {label ?? placeholder}
+      </label>
+      <input
+        name={name}
+        type="text"
+        className="py-1 px-2 rounded border border-neutral-400 hover:border-dark focus:border-dark transition-all outline-none"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onKeyUp={onKeyUp}
+        disabled={disabled}
+      />
+      {error && <p className="text-xs text-red-500">{error.message}</p>}
+    </div>
+  );
+}
