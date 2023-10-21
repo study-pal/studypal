c528f6aaffea9b212693e09e089b3601e19f581f
Krizza Bullecer <krizzabullecer@gmail.com>
2023-10-21 21:22:36 +0900

    add error codes and optimize

--- app/api/tutors/new/route.js
+++ app/api/tutors/new/route.js
@@ -1,18 +1,20 @@
 import { db } from "@/firebase";
 
 export async function POST(request) {
-  const { userID, gender, bookingLink, ageGroup, subjects } =
-    await request.json();
-  let tutor = {
-    gender: gender,
-    bookingLink: bookingLink,
-    ageGroup: ageGroup,
-    subjects: subjects,
-  };
+  const { userId, ...tutorData } = await request.json();
   try {
-    db.collection("tutors").doc(userID).set({ tutor });
-    return Response.json({ status: "success" , data: tutor });
+    db.collection("tutors").doc(userId).set({ tutorData });
+    return Response.json(
+      {
+        status: "success",
+        data: { id: userId, ...tutorData },
+      },
+      { status: 200 },
+    );
   } catch (error) {
-    return Response.json({ status: "error", message: JSON.stringify(error) });
+    return Response.json(
+      { status: "error", message: JSON.stringify(error) },
+      { status: 400 },
+    );
   }
 }
