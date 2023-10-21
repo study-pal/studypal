41e19f955ca8b1bcc953fc54b298ff0bed7a0731
egwilborn <egwilborn@gmail.com>
2023-10-21 16:13:41 -0500

    deletes user/id get request

--- app/api/users/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import { db } from "@/firebase";
-export async function GET(request, { params }) {
-  //   console.log(params.id);
-  const snapshot = await db.collection("users").doc(params.id).get();
-  const user = snapshot.data();
-  const status = "success!";
-  return Response.json({ data: user, status });
-}
