fa8e8aeee598230278bdebd5a095b16c5d3c15e0
egwilborn <egwilborn@gmail.com>
2023-10-21 19:40:52 -0500

    adds authentication middleware

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import Passage from "@passageidentity/passage-node";
+
+export async function middleware(request) {
+  const authToken = request.cookies.get("psg_auth_token")?.value;
+  console.log(authToken);
+  if (!authToken) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  const passageConfig = {
+    appID: process.env.NEXT_PUBLIC_PASSAGE_APP_ID,
+    apiKey: process.env.PASSAGE_API_KEY,
+    authStrategy: Passage.HEADER_AUTH,
+  };
+  const passage = new Passage(passageConfig);
+  const userID = await passage.validAuthToken(authToken);
+  if (!userID) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  await passage.user.signOut(userID);
+}
+
+export const config = {
+  matcher: ["/api/users/:path*", "/api/tutors/new"],
+};
