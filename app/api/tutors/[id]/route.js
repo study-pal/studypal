87fd2c1ba4b38201db1c6f6f4135168bc69434b8
Romeo Descallar Jr <romeojunior.descallar@gmail.com>
2023-10-22 16:38:39 +0800

    single tutor page api integration (#49)

--- app/api/tutors/[id]/route.js
+++ app/api/tutors/[id]/route.js
@@ -2,9 +2,16 @@ import { db } from "@/firebase";
 export async function GET(request, { params }) {
   try {
     const snapshot = await db.collection("tutors").doc(params.id).get();
-    const tutor = snapshot.data();
-    return Response.json({ status: "success", data: tutor });
+
+    if (snapshot.exists) {
+      return Response.json({ status: "success", data: {...snapshot.data().tutorData} }, {status:200});
+    } else {
+      return Response.json({ status: "error", message: "Not found" }, {status:404});
+    }
+
   } catch (error) {
-    return Response.json({ status: "error", message: JSON.stringify(error) });
+    console.log(error);
+    return Response.json({ status: "error", message: JSON.stringify(error) }, {status:500});
+
   }
 }
