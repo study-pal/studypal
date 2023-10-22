1548de77ddb5a20850bf550f79ecc7d00a92ee9b
Krizza Bullecer <krizzabullecer@gmail.com>
2023-10-22 22:26:56 +0900

    add image in zod and remo

--- schemas/tutor.js
+++ schemas/tutor.js
@@ -18,32 +18,31 @@ export const ageGroups = [
   "Adult Education",
 ];
 
-export const tutorSchema = z
-  .object({
-    gender: z.enum([
-      "Male",
-      "Female",
-      "Transgender",
-      "Non-binary/non-conforming",
-      "Prefer not to respond",
-    ]),
-    ageGroup: z.enum([
-      "Preschool",
-      "Kindergarten",
-      "Elementary School",
-      "Middle School",
-      "High School",
-      "Post-Secondary Education",
-      "Adult Education",
-    ]),
-    subjects: z
-      .array(z.object({ value: z.string() }))
-      .min(1, { message: "At least 1 Subject is required." })
-      .transform((data) => data.map((item) => item.value)),
-    bio: z.string().min(1, { message: "Bio is required." }),
-    bookingLink: z.string().min(1, { message: "Booking link is required." }),
-  })
-  .required();
+export const tutorSchema = z.object({
+  image: z.any(),
+  gender: z.enum([
+    "Male",
+    "Female",
+    "Transgender",
+    "Non-binary/non-conforming",
+    "Prefer not to respond",
+  ]),
+  ageGroup: z.enum([
+    "Preschool",
+    "Kindergarten",
+    "Elementary School",
+    "Middle School",
+    "High School",
+    "Post-Secondary Education",
+    "Adult Education",
+  ]),
+  subjects: z
+    .array(z.object({ value: z.string() }))
+    .min(1, { message: "At least 1 Subject is required." })
+    .transform((data) => data.map((item) => item.value)),
+  bio: z.string().min(1, { message: "Bio is required." }),
+  bookingLink: z.string().min(1, { message: "Booking link is required." }),
+});
 
 export const initialTutorValues = {
   gender: "Male",
