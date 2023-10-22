435ca6b792a4bef80ca153b97214aef588c47e83
Krizza Bullecer <krizzabullecer@gmail.com>
2023-10-22 18:59:59 +0900

    fix layout stylng

--- app/settings/layout.jsx
+++ app/settings/layout.jsx
@@ -1,50 +1,74 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import PencilIcon from "../icons/pencil.svg";
 import UserIcon from "../icons/user.svg";
 import AccountIcon from "../icons/account-circle.svg";
+import { getCurrentUser } from "@/actions/passageUser";
 
 function SettingsLayout({ children }) {
   const pathName = usePathname();
   const router = useRouter();
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { userInfo } = await getCurrentUser();
+      setName(
+        `${userInfo.user_metadata.first_name} ${userInfo.user_metadata.last_name}`,
+      );
+    })();
+  }, [setName]);
+
   const links = [
     {
       path: "/settings/account",
       label: "Account Settings",
-      icon: (
-        <UserIcon className="w-5 fill-dark mr-1 group-hover:fill-emerald-700" />
+      icon: (active) => (
+        <UserIcon
+          className={`w-5 mr-1 ${
+            active
+              ? "fill-emerald-700 group-hover:fill-emerald-700"
+              : " fill-dark group-hover:fill-emerald-500"
+          }`}
+        />
       ),
     },
     {
       path: "/settings/tutor",
       label: "Tutor Details",
-      icon: (
-        <PencilIcon className="w-5 fill-dark mr-1 group-hover:fill-emerald-700" />
+      icon: (active) => (
+        <PencilIcon
+          className={`w-5 mr-1 ${
+            active
+              ? "fill-emerald-700 group-hover:fill-emerald-700"
+              : " fill-dark group-hover:fill-emerald-500"
+          }`}
+        />
       ),
     },
   ];
+
   return (
-    //<div className="sm:flex m-0 md:grid gap-4 grid-cols-3 py-5 px-6 mt-10 mx-56">
     <div className="md:grid gap-2 grid-cols-4 py-5 px-6 mt-10 mx-10 lg:mx-40">
-      <div className="flex flex-col gap-3 mt-2 w-40">
+      <div className="col-start-1 col-end-2 flex flex-col gap-2 mt-2">
         <div className="text-lg font-medium flex border-b-2 border-neutral-300 pl-1 ">
           <AccountIcon className="w-5 fill-dark mr-1" />
-          <span>John Doe</span>
+          <span>{name}</span>
         </div>
         {links.map(({ path, label, icon }) => {
           const activeLinkStyle =
             pathName === path
               ? "pt-1.5 bg-emerald-100 text-emerald-700 rounded group hover:bg-emerald-200"
               : "hover:text-emerald-500";
-          // console.log(pathName);
           return (
             <div
               key={path}
               onClick={() => router.push(path)}
               className={`cursor-pointer font-medium flex py-1.5 pl-1 group ${activeLinkStyle}`}
             >
-              {icon}
+              {icon(pathName === path)}
               {label}
             </div>
           );
